1ed6b8212dd5e382b9e4d6d08ede76fa8b4c7e98
belluccaz <lucas.bellucci@icloud.com>
2025-02-15 08:10:40 -0300

    updated react classes

---
+++ React Class 5 - React Components/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Matrix.</li>
+      <li>Oppenheimer.</li>
+      <li>Interestellar.</li>
+    </ul>
+  );
+}
+
+export default List;
