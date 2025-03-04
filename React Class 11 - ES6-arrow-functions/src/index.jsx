caf05e06353cd9e9b709a72c647408665c14686d
belluccaz <lucas.bellucci@icloud.com>
2025-03-04 13:47:44 -0300

    New classes 12 and 13

--- React Class 11 - ES6-arrow-functions/src/index.jsx
+++ React Class 11 - ES6-arrow-functions/src/index.jsx
@@ -4,24 +4,24 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
-// const newNumbers = numbers.map(x => x * x);
+const newSquareNumbers = numbers.map(x => x * x);
 
 //Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map( x => x * 2);
+const newNumbers = numbers.map( x => x * 2);
 
 ////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(num => num < 10);
+const newFilteredNumbers = numbers.filter(num => num < 10);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-//const newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+const newReducedNumbers = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
 
 //Find - find the first item that matches from an array.
-// const newNumber = numbers.find(num => num > 10);
+const newFoundNumbers = numbers.find(num => num > 10);
 
 //FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(num => num > 10);
+const newIndexFoundNumbers = numbers.findIndex(num => num > 10);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
