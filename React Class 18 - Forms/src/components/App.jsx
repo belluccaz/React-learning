4cfc7adbda56c581a09abe0cefe22803bdf42976
belluccaz <lucas.bellucci@icloud.com>
2025-03-10 17:43:29 -0300

    added class 19

--- React Class 18 - Forms/src/components/App.jsx
+++ React Class 18 - Forms/src/components/App.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 
 function App() {
+  //variáveis para tratar estado do input
   const [name, setName] = useState("");
+  //variáveis para tratar estado do heading
   const [headingText, setHeading] = useState("");
 
+  //função para tratar o evento de clique no botão
   function handleClick(event) {
-    setHeading(name);
+    setHeading(name); //método para atualizar o estado do heading
 
-    event.preventDefault();
+    event.preventDefault(); //método para prevenir que a aplicação atualize ao estado inicial
   }
 
+  //função para tratar o evento de mudança no input
   function handleChange(event) {
-    setName(event.target.value);
+    setName(event.target.value); //método para atualizar o estado do input
   }
+
   return (
     <div className="container">
       <h1>Hello {headingText}</h1>
