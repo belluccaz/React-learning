08777c9664950bc619463893a5f7c871cdb76fe7
belluccaz <lucas.bellucci@icloud.com>
2025-03-11 17:51:41 -0300

    adicionando novas aulas

---
+++ React Class 21 - Spread Operator/src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue, //isso é um spread operator e serve para copiar o objeto anterior e adicionar o novo valor ao objeto copiado
+        [name]: value, //spread operator vem do ES6 e é usado para copiar valores de um objeto para outro mas também pode copiar arrays
+      };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
